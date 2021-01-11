96a850c8328f92fbcebfc5ede97116899f913827
James Scott <tlmsols50@gmail.com>
2021-01-10 20:04:43 -0500

    Home View cleanup

---
+++ WeldLearn/Extensions/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  WeldLearn
+//
+//  Created by JWSScott777 on 1/10/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
