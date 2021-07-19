5a2ea47514d92c695649ea338a3d88c651f35ef5
James Scott <tlmsols50@gmail.com>
2021-07-18 22:36:12 -0400

    Added Shared content

---
+++ WeldLearn/Community/LoadStates.swift
@@ -0,0 +1,12 @@
+//
+//  LoadStates.swift
+//  WeldLearn
+//
+//  Created by JWSScott777 on 7/18/21.
+//
+
+import Foundation
+
+enum LoadState {
+    case inactive, loading, success, noResults
+}
---
+++ WeldLearn/Community/SharedItem.swift
@@ -0,0 +1,15 @@
+//
+//  SharedItem.swift
+//  WeldLearn
+//
+//  Created by JWSScott777 on 7/18/21.
+//
+
+import Foundation
+
+struct SharedItem: Identifiable {
+    let id: String
+    let title: String
+    let detail: String
+    let completed: Bool
+}
